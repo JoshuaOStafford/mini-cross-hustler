9040b30798e2561d3dc738a2b17e40f0f2af39eb
Josh Stafford <joshuaostafford@gmail.com>
2024-09-27 09:08:46 -0700

    Formatting changes on AddPuzzle, added logic to prevent time=0 submissions

--- app/components/AddPuzzle.tsx
+++ app/components/AddPuzzle.tsx
@@ -31,8 +31,13 @@ const AddPuzzle = () => {
     const [words, setWords] = useState<string[]>([]);
     const [wordsData, setWordsData] = useState<Word[]>([]);
     const [currentStep, setCurrentStep] = useState<Step>(Step.Date);
+    const [error, setError] = useState<string | null>(null);
 
     async function handleSubmit() {
+        if (time === 0) {
+            setError("Time must be greater than 0");
+            return;
+        }
         try {
             const response = await axios.post("http://localhost:4000/puzzle", {
                 date,
@@ -52,25 +57,35 @@ const AddPuzzle = () => {
 
     return (
         <div>
-        <h1>Add Puzzle {time} for {date ? dayjs(date).format("MMMM D, YYYY") : ""}</h1>
+        <h1>Add Puzzle for {date ? dayjs(date).format("MMMM D, YYYY") : ""}</h1>
 
-        <DateSelector date={date} setDate={setDate} />
+        {error && <p>{error}</p>}
+        {currentStep === Step.Date && <DateSelector date={date} setDate={setDate} />}
 
         <Instructions currentStep={currentStep} />
 
         {(currentStep === Step.Structure || currentStep === Step.WordInfo) && <PuzzleStructure structure={structure} setStructure={setStructure} />}
         {currentStep === Step.Time && <TimeSelector time={time} setTime={setTime} />}
-        {currentStep === Step.Time && <button
-            onClick={handleSubmit}
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-        >
-            Submit Puzzle
-        </button>}
+        
 
         
         <div className="flex space-x-2">
-            {currentStep > Step.Structure && <button onClick={() => setCurrentStep(currentStep - 1)}>Back</button>}
-            {currentStep < Step.Submitted && <button onClick={() => setCurrentStep(currentStep + 1)}>Next</button>}
+            {currentStep > Step.Date && 
+                <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => setCurrentStep(currentStep - 1)}>Back</button>}
+
+            {currentStep < Step.Time && 
+                <button 
+                className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => setCurrentStep(currentStep + 1)}>Next</button>}
+
+            {currentStep === Step.Time && 
+                <button
+                    onClick={handleSubmit}
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    Submit Puzzle</button>}
         </div>
         
         </div>
